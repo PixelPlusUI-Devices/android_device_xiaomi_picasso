5af07f92ab1444e0edb7c948080f173109cf1f9d
dinhsan2000 <dinhsan200@gmail.com>
2022-01-16 17:30:35 +0700

    picasso: Bring Up PPUI 4.0

--- aosp_picasso.mk
+++ aosp_picasso.mk
@@ -11,11 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from picasso device
 $(call inherit-product, device/xiaomi/picasso/device.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common PPUI stuff.
 $(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_USES_AOSP_RECOVERY := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_INCLUDE_LIVE_WALLPAPERS := false
+
+#PixelPlus-UI Official Stuff
+CUSTOM_BUILD_TYPE := OFFICIAL
 PPUI_MAINTAINER := DinhSan
 
 # Device identifier. This must come after all inclusions.
